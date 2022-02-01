0865b63f5f4d0ea2aa0a9d82c25d0c4170cfa83e
Shwetha Prakash <itzmeshwetha@gmail.com>
2022-02-01 13:27:56 +0530

    completed

---
+++ loop.js
@@ -0,0 +1,79 @@
+//Assign1
+var count = 1;
+while (count <= 8) {
+  console.log("Shwetha");
+  count++;
+}
+
+//Assign2
+for (var limit = 3; limit <= 6; limit++) {
+  console.log("Shwetha");
+}
+
+//Assign3
+var i = 25;
+while (i >= 20) {
+  console.log(i);
+  i--;
+}
+
+//Assign4
+for (var i = 13; i >= 0; i = i - 4) {
+  console.log(i);
+}
+
+//Assign5
+var sum = 0;
+for (var i = 1; i <= 5; i++) {
+  sum = sum + i;
+}
+var average = sum / 5;
+console.log(average);
+
+//Assign6
+var sum = 0;
+for (var i = 1; i <= 100; i++) {
+  sum = sum + i;
+}
+var average = sum / 100;
+console.log(average);
+
+//Assign7
+for (var i = 3; i <= 99; i = i + 3) {
+  console.log(i);
+}
+
+//Assign8
+var strg = "edstem";
+for (var i = 0; i < strg.length; i++) {
+  if (
+    strg[i] === "d" ||
+    strg[i] === "s" ||
+    strg[i] === "t" ||
+    strg[i] === "m"
+  ) {
+    console.log(strg[i]);
+  }
+}
+
+//Assign9
+var string = "Shwetha";
+console.log(string.length);
+
+//Assign10
+var char = "Hello";
+console.log(char[0]);
+console.log(char[4]);
+
+//Assign11
+var stg = "edstem";
+for (i = 1; i < stg.length; i = i + 2) {
+  console.log(stg[i]);
+}
+
+//Assign12
+var sum = 0;
+for (var i = 1; i <= 10; i++) {
+  sum = sum + i;
+}
+console.log(sum);
